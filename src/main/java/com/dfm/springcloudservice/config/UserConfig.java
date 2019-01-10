c8db0663f3c232587cac5f83f39650c9e31e99bf
Ajith Varghese <ajith.jv.dev@gmail.com>
2019-01-10 16:36:36 -0500

    mock data added

--- src/main/java/com/dfm/springcloudservice/config/UserConfig.java
+++ src/main/java/com/dfm/springcloudservice/config/UserConfig.java
@@ -1,11 +1,39 @@
 package com.dfm.springcloudservice.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
+
+import com.dfm.springcloudservice.model.User;
 
 @Configuration
 public class UserConfig {
 
-	@Bean
+	public List<User> getUsersList(){
+		List<User> l = new ArrayList<User>();
+		l.add(createUser("1", "Ajith", "Varghese", 40, "Male"));
+		l.add(createUser("2", "John", "Thomas", 34, "Male"));
+		l.add(createUser("3", "Tom", "Nixon", 44, "Male"));
+		l.add(createUser("4", "Linda", "Patterson", 50, "Female"));
+		return(l);
+	}
 	
+	public User createUser(String id, String firstName, String lastName, int age, String gender) {
+		User u = new User();
+		u.setAge(age);
+		u.setEmployeeId(id);
+		u.setFirstName(firstName);
+		u.setGender(gender);
+		u.setLastName(lastName);
+		return(u);
+	}
 	
+	public User getUser(String id) {
+		User u = getUsersList().stream()
+							.filter(user -> user.getEmployeeId().equals(id))
+							.findAny()
+			                .orElse(null);
+		return(u);
+	}
 }
--- src/main/java/com/dfm/springcloudservice/controller/UserDetails.java
+++ src/main/java/com/dfm/springcloudservice/controller/UserDetails.java
@@ -1,22 +1,20 @@
 package com.dfm.springcloudservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dfm.springcloudservice.config.UserConfig;
 import com.dfm.springcloudservice.model.User;
 
 @RestController
 public class UserDetails {
-
+	@Autowired
+	UserConfig userConfig;
+	
 	@GetMapping("/user/{id}")
 	User getUserDetails(@PathVariable String id) {
-		User u = new User();
-		u.setAge(40);
-		u.setEmployeeId(id);
-		u.setFirstName("Ajith");
-		u.setGender("Male");
-		u.setLastName("Varghese");
-		return(u);
+		return(userConfig.getUser(id));
 	}
 }
