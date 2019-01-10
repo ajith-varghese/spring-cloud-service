9f24a21cac98966ab6b09cfe7a8b26dfdc3503f4
Ajith Varghese <ajith.jv.dev@gmail.com>
2019-01-10 15:53:41 -0500

    gitignore updated

---
+++ src/main/java/com/dfm/springcloudservice/SpringCloudServiceApplication.java
@@ -0,0 +1,14 @@
+package com.dfm.springcloudservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudServiceApplication.class, args);
+	}
+
+}
+
