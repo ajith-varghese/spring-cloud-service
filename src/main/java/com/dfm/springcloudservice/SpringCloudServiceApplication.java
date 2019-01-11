cf42fc162c5737c6c5c26345d1dc7a174712a3fc
Ajith Varghese <ajith.jv.dev@gmail.com>
2019-01-11 09:34:45 -0500

    configured discovery client

--- src/main/java/com/dfm/springcloudservice/SpringCloudServiceApplication.java
+++ src/main/java/com/dfm/springcloudservice/SpringCloudServiceApplication.java
@@ -1,9 +1,13 @@
 package com.dfm.springcloudservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableDiscoveryClient
 public class SpringCloudServiceApplication {
 
 	public static void main(String[] args) {
